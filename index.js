ae749a8c4c8d137632303d9ba2409a89f06ee269
Suei43 <sueiraphael@gmail.com>
2023-05-03 22:12:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Running!")
+var Twit = require('twit');
+
+var T = new Twit({
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
+})
